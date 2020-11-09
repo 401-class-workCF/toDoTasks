554d9110d8c8b0c8d91317ddd194f73bb70ce728
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-11-09 09:26:33 -0800

    added some styling

--- src/components/to-do/List/index.js
+++ src/components/to-do/List/index.js
@@ -3,15 +3,19 @@ import React from 'react';
 class TodoList extends React.Component {
   render() {
     return (
-      <ul>
-        {this.props.list.map((item) => (
-          <li key={item._id}>
-            <span onClick={() => this.props.handleComplete(item._id)}>
-              {item.text}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div class='card'>
+        <h4 class='card-header'>Tasks</h4>
+        <ul class='list-group list-group-flush'>
+          {this.props.list.map((item) => (
+            <li class='list-group-item' key={item._id}>
+              <span onClick={() => this.props.handleComplete(item._id)}>
+                {item.text}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <h5 class='card-footer'>End of Tasks List</h5>
+      </div>
     );
   }
 }
