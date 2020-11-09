4e722fb400f50fec5c4480187fa4db5673a40f5f
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-11-09 08:42:26 -0800

    fixing imports

--- src/components/to-do/Form/index.js
+++ src/components/to-do/Form/index.js
@@ -61,32 +61,3 @@ class Form extends React.Component {
 
 export default Form;
 
-// import React from 'react';
-// import Form from 'react-bootstrap/Form';
-// import Card from 'react-bootstrap/Card';
-// // import Button from 'react-bootstrap/Button';
-// // import '../App.scss';
-// // import useForm from '../../hooks/formH';
-// // import Auth from '../auth/auth';
-
-// const AddTask = (props) => {
-// //   const [handleInputChange, handleSubmit] = useForm(formCallback);
-
-// //   function formCallback(value) {
-// //     props.handleSubmit(value);
-// //   }
-
-//   return (
-//     <>
-//       {/* <Auth capabilty="update"> */}
-//       <Form>
-//         <Card>
-
-//         </Card>
-//       </Form>
-//       {/* </Auth> */}
-//     </>
-//   );
-// };
-
-// export default AddTask;
