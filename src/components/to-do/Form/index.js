948e9abd0b8a3813faa45f2744a96ff5f5c4bed6
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-11-09 06:58:56 -0800

    ...

---
+++ src/components/to-do/Form/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+// import '../App.scss';
+// import useForm from '../../hooks/formH';
+// import Auth from '../auth/auth';
+
+const AddTask = (props) => {
+//   const [handleInputChange, handleSubmit] = useForm(formCallback);
+
+//   function formCallback(value) {
+//     props.handleSubmit(value);
+//   }
+
+  return (
+    <>
+      {/* <Auth capabilty="update"> */}
+      <Form>
+        <Card>
+
+        </Card>
+      </Form>
+      {/* </Auth> */}
+    </>
+  );
+};
+
+export default AddTask;
